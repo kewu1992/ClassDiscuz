5c6337fef2938f2af452d027b1e2f3f14763b153
Zhongchuan Xu <zhongchx@andrew.cmu.edu>
2015-11-06 17:35:28 -0500

    zhongyu cheng le

--- app/src/main/java/cmu/banana/classdiscuz/ui/LoginActivity.java
+++ app/src/main/java/cmu/banana/classdiscuz/ui/LoginActivity.java
@@ -32,6 +32,8 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import cmu.banana.classdiscuz.R;
+
 import static android.Manifest.permission.READ_CONTACTS;
 
 /**
