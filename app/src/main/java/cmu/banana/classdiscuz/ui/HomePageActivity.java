fbb98fa1b123eb481f2b0d737c069fbe79b2f84d
Chaoya Li <lichaoya@outlook.com>
2015-11-06 22:38:27 -0500

    commit

--- app/src/main/java/cmu/banana/classdiscuz/ui/HomePageActivity.java
+++ app/src/main/java/cmu/banana/classdiscuz/ui/HomePageActivity.java
@@ -1,13 +1,19 @@
 package cmu.banana.classdiscuz.ui;
 
 import android.app.ActionBar;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import cmu.banana.classdiscuz.ChatPageFragment;
 import cmu.banana.classdiscuz.R;
+import cmu.banana.classdiscuz.SchedulePageFragment;
 
 /**
  * Created by WK on 11/6/15.
@@ -19,10 +25,19 @@ public class HomePageActivity extends AppCompatActivity {
     private TextView memberNumTextView;
     private ListView chatHistoryListView;
     private EditText textEditText;
+    private TextView scheduleClick;
+    private TextView chatClick;
+
+    private SchedulePageFragment schedule;
+    private ChatPageFragment chat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+ //       requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_homepage);
 
         courseNameTextView = (TextView) findViewById(R.id.chat_course_name_text_view);
@@ -32,7 +47,56 @@ public class HomePageActivity extends AppCompatActivity {
         chatHistoryListView = (ListView) findViewById(R.id.chat_list_view);
         textEditText = (EditText) findViewById(R.id.chat_edit_text);
 
-        String text = textEditText.getText().toString();
+        scheduleClick = (TextView) findViewById(R.id.textView8);
+        chatClick = (TextView) findViewById(R.id.textView9);
+
+//        String text = textEditText.getText().toString();
+
+        setDefaultFragment();
 
     }
+
+    private void setDefaultFragment()
+    {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        schedule = new SchedulePageFragment();
+        transaction.replace(R.id.homepage_content, schedule);
+        transaction.commit();
+    }
+
+    public void onClick(View v) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+
+        switch (v.getId())
+        {
+            //click the schecule
+            case R.id.textView8:
+                if (schedule == null)
+                {
+                    schedule = new SchedulePageFragment();
+                }
+                //
+                transaction.replace(R.id.homepage_content, schedule);
+                break;
+            case R.id.textView9:
+                if (chat == null)
+                {
+                    chat = new ChatPageFragment();
+                }
+                transaction.replace(R.id.homepage_content, chat);
+                break;
+        }
+        // transaction.addToBackStack();
+        // 事务提交
+        transaction.commit();
+    }
+    public void onNavFragmentInteraction(String string) {
+        // Do stuff
+    }
+
+
+
 }
+
