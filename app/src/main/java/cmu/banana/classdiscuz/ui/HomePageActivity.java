b6d3348a88c5384afeeaef174e72097655ac8a18
KeWu <kewu@andrew.cmu.edu>
2015-12-04 10:12:04 -0500

    add services for char

--- app/src/main/java/cmu/banana/classdiscuz/ui/HomePageActivity.java
+++ app/src/main/java/cmu/banana/classdiscuz/ui/HomePageActivity.java
@@ -21,14 +21,6 @@ import cmu.banana.classdiscuz.R;
  * Created by WK on 11/6/15.
  */
 public class HomePageActivity extends AppCompatActivity implements ChatPageFragment.OnFragmentInteractionListener, SchedulePageFragment.OnFragmentInteractionListener{
-    private TextView courseNameTextView;
-    private ListView courseListView;
-    private ListView memberListView;
-    private TextView memberNumTextView;
-    private ListView chatHistoryListView;
-    private EditText textEditText;
-    private TextView scheduleClick;
-    private TextView chatClick;
 
     private SchedulePageFragment schedule;
     private ChatPageFragment chat;
@@ -38,25 +30,9 @@ public class HomePageActivity extends AppCompatActivity implements ChatPageFragm
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
- //       requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_homepage);
 
-        courseNameTextView = (TextView) findViewById(R.id.chat_course_name_text_view);
-        courseListView = (ListView) findViewById(R.id.courseListView);
-        memberListView = (ListView) findViewById(R.id.memberListView);
-        memberNumTextView = (TextView) findViewById(R.id.memberNumTextView);
-        chatHistoryListView = (ListView) findViewById(R.id.chat_list_view);
-        textEditText = (EditText) findViewById(R.id.chat_edit_text);
-
-        scheduleClick = (TextView) findViewById(R.id.bottom_switch_schedule);
-        chatClick = (TextView) findViewById(R.id.bottom_switch_chat);
-
-//        String text = textEditText.getText().toString();
-
         setDefaultFragment();
-
     }
 
     private void setDefaultFragment()
