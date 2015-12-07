d6fe940cd8ea99ef8d1f50f79d06af4157eb4aa5
KeWu <kewu@andrew.cmu.edu>
2015-12-06 22:15:31 -0500

    hide action bar of frontpage

--- app/src/main/java/cmu/banana/classdiscuz/ui/FrontpageActivity.java
+++ app/src/main/java/cmu/banana/classdiscuz/ui/FrontpageActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,6 +22,9 @@ public class FrontpageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frontpage);
+
+        ActionBar actionBar =  getSupportActionBar();
+        actionBar.hide();
 
         signUpBtn = (Button) findViewById(R.id.frontpage_signup);
         signInBtn = (Button) findViewById(R.id.frontpage_login);
